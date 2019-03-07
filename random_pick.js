2a9febaeacde79616c61618b1d571620a34cd524
Junghyun Park <rockethyun@gmail.com>
2019-03-07 12:40:51 +0900

    Update random_pick.js

--- random_pick.js
+++ random_pick.js
@@ -1,5 +1,9 @@
 const comments = [];
-$("div[data-sigil=comment-body]").each((i,comment_body) => comments.push($(comment_body).siblings()[0]));
-comments.sort( function() { return 0.5 - Math.random() } );
-const selected_users = comments.slice(0, 10);
+const comment_bodys = $("div[data-sigil=comment-body]");
+comment_bodys.each((index, comment_body)=> {
+  const comment_name = $(comment_body).siblings()[0];
+  comments.push(comment_name); 
+});
+const shuffled_comments = comments.sort( function() { return 0.5 - Math.random() } );
+const selected_users = shuffled_comments.slice(0,10);
 selected_users.forEach((selected_user) => window.open("https://www.facebook.com" + $(selected_user).find("a").attr("href"), "_blank"));
